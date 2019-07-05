df7667ec6313d0445be9e0e818d68f3ddc80742b
Nap Ramirez <napramirez@gmail.com>
2019-07-05 16:00:40 +0800

    Fixed issue when posting no data

--- dummy-web-server.py
+++ dummy-web-server.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#
 
 import socket, os
 from http.server import BaseHTTPRequestHandler, HTTPServer
@@ -8,24 +9,21 @@ hostName = ""
 hostPort = int(os.getenv("HTTP_PORT", 8080))
 
 httpGetResponseBody = os.getenv("HTTP_GET_RESPONSE_BODY", "HTTP OK")
-httpPostResponseBody = os.getenv("HTTP_POST_RESPONSE_BODY", "")
+httpPostResponseBody = os.getenv("HTTP_POST_RESPONSE_BODY", "HTTP Accepted")
 
 class MyServer(BaseHTTPRequestHandler):
 
-	#	GET is for clients geting the predi
 	def do_GET(self):
 		self.send_response(200)
 		self.wfile.write(bytes(httpGetResponseBody, "utf-8"))
 
-	#	POST is for submitting data.
 	def do_POST(self):
+		print( "Incoming http: ", self.path )
 
-		print( "incomming http: ", self.path )
-
-		content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-		post_data = self.rfile.read(content_length) # <--- Gets the data itself
-
-		print( "POST data: ", post_data.decode("utf-8") )
+		if self.headers['Content-Length'] is not None:
+			content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+			post_data = self.rfile.read(content_length) # <--- Gets the data itself
+			print( "POST data: ", post_data.decode("utf-8") )
 
 		self.send_response(204)
 		self.wfile.write(bytes(httpPostResponseBody, "utf-8"))
@@ -40,4 +38,3 @@ except KeyboardInterrupt:
 
 myServer.server_close()
 print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
-
